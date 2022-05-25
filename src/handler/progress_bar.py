ed1c64b53a6f5ba0b72d162cd79147442c54c98a
Nikita <nvs38@tpu.ru>
2022-05-25 16:37:47 +0700

    line cleaning added  and fixed progress share displaying

--- src/handler/progress_bar.py
+++ src/handler/progress_bar.py
@@ -14,8 +14,8 @@ class Progress_bar(Handler):
     def print_res(self):
         progress_filled = self.color + int(self.progress*self.LENGTH)*' ' + Back.RESET
         not_filled = int(self.LENGTH-int(self.progress*self.LENGTH))*"."
-        template = f"\r|{progress_filled}{not_filled}| {self.progress*100}% | {self.current_step}/{self.steps} - ./{self.file}"
-        # self.clear_line()
+        template = f"\r|{progress_filled}{not_filled}| {self.progress*100:.1f}% | {self.current_step}/{self.steps} - ./{self.file}" #! there is a little bug for long file namespacing
+        self.clear_line()
         print(template, end="\r")
     
     def clear_line(self):
