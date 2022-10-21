e3cdbf98f6a2bbd81ad4d34da350ade8d0ba0cec
Nikita <nvs38@tpu.ru>
2022-10-21 14:31:43 +0700

    minor change in cli test

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -22,10 +22,7 @@ def test_status():
 
 def test_extract():
     path = PATH(test_data_keff_fin)
-    # path:str = os.path.join(os.path.split(os.path.dirname(__file__))[0], test_data)
-
     asyncio.run(CLI().extract(code="keff", extension=".FIN"))
-    # print(cli.on_clear)
     assert 0
 
 @pytest.mark.asyncio
